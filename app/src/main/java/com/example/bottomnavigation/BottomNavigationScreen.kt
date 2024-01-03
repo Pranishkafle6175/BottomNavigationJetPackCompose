bf159f60b283730c790fac9bffc96c4817ed5d67
Pranish <pranishkafle1@gmail.com>
2024-01-03 18:29:38 +0545

    Sealed Class for Bottom Navigation

--- app/src/main/java/com/example/bottomnavigation/BottomNavigationScreen.kt
+++ app/src/main/java/com/example/bottomnavigation/BottomNavigationScreen.kt
@@ -1,2 +1,31 @@
 package com.example.bottomnavigation
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.ui.graphics.vector.ImageVector
+
+sealed class BottomNavigationScreen(
+    var route:String,
+    var title:String,
+    var icon:ImageVector
+){
+    object HomeScreen : BottomNavigationScreen(
+        route = "home",
+        title = "Home",
+        icon = Icons.Default.Home
+    )
+
+    object ProfileScreen : BottomNavigationScreen(
+        route = "profile",
+        title = "Profile",
+        icon = Icons.Default.Person
+    )
+
+    object Setting : BottomNavigationScreen(
+        route = "setting",
+        title = "Setting",
+        icon = Icons.Default.Settings
+    )
+}
